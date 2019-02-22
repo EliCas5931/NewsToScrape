414b25a744d92a04878e588cca7ce4441ddb11b8
Elicas5931 <elizabeth.perry.339@gmail.com>
2019-02-22 09:37:52 -0500

    Working on app.js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 
 
-$.getJSON("/all", function(data) {
+$.getJSON("/all", function (data) {
     console.log(data);
     for (var i = 0; i < data.length; i++) {
         $("#results").append(
@@ -9,10 +9,16 @@ $.getJSON("/all", function(data) {
     }
 });
 
-$("#scrape").on("click", function() {
-
+$("#scrape").on("click", function () {
+    $.getJSON("/scrape", function (data) {
+        for (var i = 0; i < data.length; i++) {
+            $("#results").append(
+                "<tr><td>" + data[i].title + "</td>" +
+                "<td>" + data[i].link + "</td></tr>");
+        }
+    })
 });
 
-$("#clear").on("click", function() {
-
+$("#clear").on("click", function () {
+    
 })
